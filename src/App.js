8509a7276048ee03a44590fe6fd2fa544dbd0d2c
Janet Sandra Marquewz Taipe <jsmarquezt@gmail.com>
2022-10-27 19:35:57 -0500

    v2 my-app1 Proyecto React Se modifico el nombre

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React- janet
+          Learn React- janet marquez
         </a>
       </header>
     </div>
